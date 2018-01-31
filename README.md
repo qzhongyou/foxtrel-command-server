82881afa64b78ef2696ca05fb3349204a38e71b2
阙中友 <quezhongyou@zhubajie.com>
2018-01-31 23:44:28 +0800

    实现es6编写服务端代码和自动刷新

--- README.md
+++ README.md
@@ -1,4 +1,7 @@
-
 ## foxtrel-command-server
+   > foxtral server
+   
+### 运行说明
 
-开发中...
+点击[foxtrel](https://github.com/qzhongyou/foxtrel/blob/master/README.md)到文档说明
+   
--- server.js
+++ server.js
@@ -15,14 +15,25 @@ exports.desc = 'launch a node server';
 
 exports.register = function (commander) {
     commander
-        .option('-p, --port <int>', 'server listen port', parseInt, 8080)
         .option('-e,--env <names>', 'compile environment', String, 'dev')
+        .option('-d, --dest <path>', 'output destination', String, '')
+        .option('-f,--file <filename>', 'config file', String, 'foxtrel.config.js')
+        .option('-r, --root <names>', 'set project root', String, process.cwd())
         .action(server);
 }
 
 
 function server(command, options) {
 
-    require('./lib/boot')(command, options);
+    //设置项目根目录
+    foxtrel.project.setProjectRoot(options.root);
 
+    foxtrel.config.set('serverFile', command);
+    const gulp = require('./lib/gulp');
+
+    if (process.env.NODE_ENV != 'product') {
+        gulp.start('watch');
+    } else {
+        gulp.start('build');
+    }
 }
